c0c16a9fc42e7a4946758af0cbe0b22e2d20922e
0xlilith <0xlilith@protonmail.com>
2021-07-13 12:41:48 +0530

    added function - disableRawEcho

---
+++ novim.c
@@ -0,0 +1,32 @@
+/*
+ * Totally not a Vim ! Not even close to it.....it's novim
+ */
+
+
+#include <stdlib>
+#include <unistd.h>
+#include <termios.h>
+
+struc termios orig_termios;
+
+void disableRawMode(){ /* turining off echo attribute when quiting the program */
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){  /* turning on echo attribute/raw mode. Echo's out the keystrokes back to the terminal  */
+	tcgetattr(STDIN_FILENO, &orig_termios);    //  tcgetattr() -> read the current attribute of the terminal
+	atexit(disableRawMode);
+	
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO);					   //  turning on the ECHO
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  //  tcsetattr()  -> writes the new attribute back to the terminal
+}
+
+int main(){
+	enableRawMode();
+
+	char c;
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	return 0;
+}
